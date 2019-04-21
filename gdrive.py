4c5694a815b1f8f67f9906d3325b3c34852718bb
rookiepeng <rookiepeng@gmail.com>
2019-04-21 14:29:05 -0400

    google drive create folder and download files

--- gdrive.py
+++ gdrive.py
@@ -1,16 +1,17 @@
 from __future__ import print_function
 import pickle
 import os.path
+import io
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 
 class GDrive():
     def __init__(self):
         SCOPES = ['https://www.googleapis.com/auth/drive']
-        
+
         creds = None
         # The file token.pickle stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
@@ -29,32 +30,66 @@ class GDrive():
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
-    
+
         self.service = build('drive', 'v3', credentials=creds)
-        
+
     def get_file_list(self):
-         # Call the Drive v3 API
-        filelist = self.service.files().list(q="trashed=False and 'root' in parents", pageSize=10, fields="nextPageToken, files(mimeType, id, name, parents)").execute()
-        #return filelist.get('files', [])
+        filelist = []
+        page_token = None
+
+        while True:
+            response = self.service.files().list(
+                q="trashed = False and 'root' in parents",
+                fields='nextPageToken, files(name, id, mimeType)',
+                pageToken=page_token).execute()
+
+            filelist = filelist + response.get('files', [])
+            page_token = response.get('nextPageToken', None)
+
+            if page_token is None:
+                break
+
         return filelist
-        
+
+    def creat_folder(self):
+        file_metadata = {
+            'name': 'edenbridge',
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': ['root']
+        }
+        file = self.service.files().create(
+            body=file_metadata, fields='name, id').execute()
+        return file
+
     def upload(self, localfile, mimetype, name_on_gdrive, parents=None):
         if parents is not None:
             file_metadata = {'name': name_on_gdrive, 'parents': [parents]}
         else:
             file_metadata = {'name': name_on_gdrive}
         media = MediaFileUpload(localfile, mimetype=mimetype)
-        file = self.service.files().create(body=file_metadata,
-                                             media_body=media,
-                                             fields='id').execute()
-        return file.get('id')
+        file = self.service.files().create(
+            body=file_metadata, media_body=media, fields='name, id').execute()
+        return file
+    
+    def download(self):
+        file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
+        request = self.service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print( "Download %d%%." % int(status.progress() * 100))
+
 
 def main():
-    gdrive = GDrive()   
+    gdrive = GDrive()
     #gdrive.upload('edenbridge.py', 'plain/text', 'edenbridge.py')
-    
+
     items = gdrive.get_file_list()
-#
+
+    folder = gdrive.creat_folder()
+    #
     if not items:
         print('No files found.')
     else:
@@ -62,7 +97,6 @@ def main():
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
 
-    
 
 if __name__ == '__main__':
     main()
