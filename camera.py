09ca9779e28e9349e957cfc1d47eab5c5ba24b29
rookiepeng <rookiepeng@gmail.com>
2019-04-28 20:12:51 -0400

    return when a stop signal is received

--- camera.py
+++ camera.py
@@ -138,11 +138,12 @@ class Camera(Thread):
                         self.q2cloud.put(copy.deepcopy(self.cmd_upload_h264))
 
                         logging.info('Stop recording')
-                        break
+                        return 1
                     else:
                         self.motion2camera.task_done()
                         logging.warning('Wrong command, continue recording')
                     pass
+            return 0
 
         date_str = datetime.datetime.now().strftime('%Y-%m-%d')
         time_str = datetime.datetime.now().strftime('%H-%M-%S')
@@ -164,7 +165,8 @@ class Camera(Thread):
                             use_video_port=True)
         self.q2mbot.put(copy.deepcopy(self.cmd_send_jpg))
 
-        take_photo_during_recording(0, date_str, time_str)
+        if take_photo_during_recording(0, date_str, time_str) == 1:
+            return
 
         if count > 1:
 
@@ -184,7 +186,9 @@ class Camera(Thread):
 
                 self.q2cloud.put(temp_cmd)
 
-                take_photo_during_recording(video_idx, date_str, time_str)
+                if take_photo_during_recording(video_idx, date_str,
+                                               time_str) == 1:
+                    return
 
             self.camera.stop_recording()
             self.q2cloud.put(copy.deepcopy(self.cmd_upload_h264))
