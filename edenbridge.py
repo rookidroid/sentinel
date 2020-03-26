c065473851a46031cc499ef21ed674ec694667f6
rookiepeng <rookiepeng@gmail.com>
2020-03-25 22:55:16 -0400

    test

--- edenbridge.py
+++ edenbridge.py
@@ -24,6 +24,7 @@ from camera import Camera
 from cloud import Cloud
 import json
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import time
 
 
 def get_config():
@@ -55,24 +56,27 @@ def main():
             q2camera.put({'cmd': 'take_video', 'count': 1})
 
     # motion = Motion(config['motion'], q2camera)
-    my_bot = MessageBot(config['bot'], q2mbot)
+    # my_bot = MessageBot(config['bot'], q2mbot)
     camera = Camera(config['camera'], q2camera, q2mbot, q2cloud)
     # cloud = Cloud(config['cloud'], q2cloud)
 
     # motion.start()
-    my_bot.start()
+    # my_bot.start()
     camera.start()
     # cloud.start()
 
-    updater = Updater(token)
-    dp = updater.dispatcher
+    while True:
+        time.sleep(1)
 
-    dp.add_handler(CommandHandler('hello', hello))
-    # dp.add_handler(CommandHandler('photo', take_photo))
-    # dp.add_handler(CommandHandler('video', take_video))
+    # updater = Updater(token)
+    # dp = updater.dispatcher
 
-    updater.start_polling()
-    updater.idle()
+    # dp.add_handler(CommandHandler('hello', hello))
+    # # dp.add_handler(CommandHandler('photo', take_photo))
+    # # dp.add_handler(CommandHandler('video', take_video))
+
+    # updater.start_polling()
+    # updater.idle()
 
 
 if __name__ == '__main__':
