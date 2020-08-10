253b7ee3050fad762cfbd5936a63d7b3cb55ecb2
rookiepeng <rookiepeng@gmail.com>
2020-08-09 22:58:09 -0400

    fix updater

--- telegram_updater.py
+++ telegram_updater.py
@@ -27,13 +27,13 @@ import socket
 
 def main():
     # argument parser
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-c", "--conf", required=True,
-    #                 help="path to the JSON configuration file")
-    # args = vars(ap.parse_args())
-    # config = json.load(open(args["conf"]))
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--conf", required=True,
+                    help="path to the JSON configuration file")
+    args = vars(ap.parse_args())
+    config = json.load(open(args["conf"]))
 
-    config = json.load(open('./front_door.json'))
+    # config = json.load(open('./front_door.json'))
 
     token = config['bot']['bot_token']
     chat_id = config['bot']['chat_id']
