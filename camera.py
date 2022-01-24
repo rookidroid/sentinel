fc83a5ce184a5eb6702275c44f0fe6c8a2997d5e
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-01-24 08:24:55 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -23,9 +23,11 @@ import socket
 
 from pathlib import Path
 import os
-import picamera
-from picamera.array import PiRGBArray
+# import picamera
+# from picamera.array import PiRGBArray
 import datetime
+
+import subprocess
 
 import copy
 import logging
@@ -54,7 +56,6 @@ class Camera():
         self.photo_path = Path(config['photo_path'])
 
         self.camera_config = config['camera']
-        # self.cwd = Path().absolute()
 
         self.max_photo_count = self.camera_config['max_photo_count']
         self.period = self.camera_config['period']
@@ -79,16 +80,16 @@ class Camera():
         # initialize the camera and grab a reference to the raw camera capture
         # camera.resolution = tuple(conf["resolution"])
         # camera.framerate = conf["fps"]
-        self.camera = picamera.PiCamera(resolution=self.det_resolution)
-        self.raw_capture = PiRGBArray(
-            self.camera,
-            size=self.det_resolution)
+        # self.camera = picamera.PiCamera(resolution=self.det_resolution)
+        # self.raw_capture = PiRGBArray(
+        #     self.camera,
+        #     size=self.det_resolution)
 
         # allow the camera to warmup, then initialize the average frame, last
         # uploaded timestamp, and frame motion counter
-        logging.info('Camera warming up')
-        time.sleep(self.camera_config["camera_warmup_time"])
-        self.avg_capture = None
+        # logging.info('Camera warming up')
+        # time.sleep(self.camera_config["camera_warmup_time"])
+        # self.avg_capture = None
 
         try:
             os.makedirs(self.video_path)
@@ -136,10 +137,14 @@ class Camera():
                     photo_idx)
             self.cmd_send_jpg['server'] = 'telegram'
 
-            self.camera.capture(str(
+            subprocess.run('libcamera-still -o '+str(
                 self.photo_path /
                 (self.cmd_send_jpg['file_name'] +
-                    self.cmd_send_jpg['extension'])))
+                    self.cmd_send_jpg['extension']))+' --shutter 100000 --gain 1 --awbgains 1,1 --immediate')
+            # self.camera.capture(str(
+            #     self.photo_path /
+            #     (self.cmd_send_jpg['file_name'] +
+            #         self.cmd_send_jpg['extension'])))
             # self.q2mbot.put(copy.deepcopy(self.cmd_send_jpg))
             self.send_bot(copy.deepcopy(self.cmd_send_jpg))
 
@@ -194,96 +199,6 @@ class Camera():
 
         # self.q2cloud.put(copy.deepcopy(self.cmd_upload_h264))
         self.send_cloud(copy.deepcopy(self.cmd_upload_h264))
-
-    # def motion_detection(self):
-    #     self.camera.resolution = self.det_resolution
-    #     self.avg_capture = None
-    #     self.motion_frame_counter = 0
-    #     is_occupied = False
-
-    #     # capture frames from the camera
-    #     for frm in self.camera.capture_continuous(
-    #             self.raw_capture, format="bgr", use_video_port=True):
-    #         # grab the raw NumPy array representing the image and initialize
-    #         # the timestamp and occupied/unoccupied text
-    #         raw_frame = frm.array
-
-    #         # clear the stream in preparation for the next frame
-    #         self.raw_capture.truncate(0)
-
-    #         # resize the frame, convert it to grayscale, and blur it
-    #         frame = imutils.resize(raw_frame, width=500)
-    #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #         gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-    #         # if the average frame is None, initialize it
-    #         if self.avg_capture is None:
-    #             # if self.motion_frame_counter < 2:
-    #             #     self.motion_frame_counter += 1
-    #             # print("[INFO] starting background model...")
-    #             self.avg_capture = gray.copy().astype("float")
-    #             # self.raw_capture.truncate(0)
-    #             continue
-
-    #         # accumulate the weighted average between the current frame and
-    #         # previous frames, then compute the difference between the current
-    #         # frame and running average
-    #         cv2.accumulateWeighted(gray, self.avg_capture, 0.5)
-    #         frame_delta = cv2.absdiff(
-    #             gray, cv2.convertScaleAbs(self.avg_capture))
-
-    #         # threshold the delta image, dilate the thresholded image to fill
-    #         # in holes, then find contours on thresholded image
-    #         thresh = cv2.threshold(frame_delta, self.delta_thresh, 255,
-    #                                cv2.THRESH_BINARY)[1]
-    #         thresh = cv2.dilate(thresh, None, iterations=2)
-    #         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-    #                                     cv2.CHAIN_APPROX_SIMPLE)
-    #         contours = imutils.grab_contours(contours)
-
-    #         # loop over the contours
-    #         for contr in contours:
-    #             # if the contour is too small, ignore it
-    #             if cv2.contourArea(contr) < self.min_area:
-    #                 continue
-
-    #             scale_factor = self.det_resolution[0]/500
-    #             date_str = datetime.datetime.now().strftime('%Y-%m-%d')
-    #             time_str = datetime.datetime.now().strftime('%H-%M-%S')
-    #             # draw box and timestamp on frame
-    #             (x, y, w, h) = cv2.boundingRect(contr)
-    #             cv2.rectangle(raw_frame,
-    #                           (int(scale_factor*x),
-    #                            int(scale_factor*y)),
-    #                           (int(scale_factor*(x + w)),
-    #                            int(scale_factor*(y + h))),
-    #                           (0, 255, 0),
-    #                           1)
-    #             cv2.putText(raw_frame, 'Front Door', (10, 25),
-    #                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 1)
-    #             cv2.putText(raw_frame, date_str + '_' + time_str,
-    #                         (10, raw_frame.shape[0] - 10),
-    #                         cv2.FONT_HERSHEY_SIMPLEX,
-    #                         0.5, (0, 0, 255), 1)
-
-    #             self.cmd_send_jpg['date'] = date_str
-    #             self.cmd_send_jpg['time'] = time_str
-    #             self.cmd_send_jpg['file_name'] = date_str + '_' + time_str
-    #             self.cmd_send_jpg['server'] = 'telegram'
-
-    #             cv2.imwrite(str(self.photo_path /
-    #                             (self.cmd_send_jpg['file_name'] +
-    #                              self.cmd_send_jpg['extension'])),
-    #                         raw_frame)
-    #             # self.q2mbot.put(copy.deepcopy(self.cmd_send_jpg))
-    #             self.send_bot(copy.deepcopy(self.cmd_send_jpg))
-
-    #             is_occupied = True
-    #             return is_occupied
-
-    #         self.motion_frame_counter += 1
-    #         if self.motion_frame_counter >= 5:
-    #             return is_occupied
 
     def run(self):
         logging.info('Camera thread started')
