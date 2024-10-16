05945e0a5de493e9191d84b75b6326af046e523c
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-10-15 22:12:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 
+echo "create service files"
 python3 create_services.py
 
+echo "copy service files to /lib/systemd/system"
 service_dir=./service/
-
 sudo cp "$service_dir"/* /lib/systemd/system/
 sudo systemctl daemon-reload
 
+echo "enable services"
 for entry in "$service_dir"/*
 do
   echo "$entry"
